f3a4a62af414ab572e236ba3899309acd019ac56
gucheng <guchengf@gmail.com>
2016-05-20 10:13:00 +0800

    Initial Commit

---
+++ index.mjs
@@ -0,0 +1,103 @@
+try {
+  var fetch = require('node-fetch');
+} catch(error) {
+  throw('Current environment doesn\'t supoort Fetch method');
+}
+
+class FetchQL {
+  /**
+   *
+   * @param {String} url - API 地址
+   */
+  constructor({ url }) {
+    this.requestObject = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      credentials: 'same-origin'
+    };
+
+    this._url = url;
+  }
+
+  /**
+   *
+   * @param {String} operationName
+   * @param {String} query
+   * @param {Object} variables
+   * @returns {Promise}
+   */
+  query({ operationName, query, variables }) {
+    let options = Object.assign({}, this.requestObject);
+    let body = {
+      operationName,
+      query,
+      variables: JSON.stringify(variables)
+    };
+    options.body = JSON.stringify(body);
+    return fetch(this._url, options)
+      .then(res => res.json())
+      .then(response => (
+        new Promise((resolve, reject) => {
+          if (response.errors && response.errors.length) {
+            reject(response);
+          }
+          resolve(response);
+        })
+      ));
+  }
+
+  /**
+   * 获取当前的 url
+   * @returns {String}
+   */
+  getUrl() {
+    return this._url;
+  }
+
+  /**
+   * 设置新的 url
+   * @param {String} url
+   */
+  setUrl(url) {
+    this._url = url;
+  }
+
+  /**
+   * 获取 ENUM 类型的 type 对象
+   * @param EnumNameList - ENUM 对象 name 数组
+   * @returns {Promise}
+   */
+  getEnumTypes(EnumNameList) {
+    let EnumTypeQuery = EnumNameList.map(type => (
+      `${type}: __type(name: "${type}") {
+          kind
+          description
+          enumValues{
+            name
+            description
+          }
+        }`
+    ));
+    let query = `
+      query {
+        ${EnumTypeQuery.join('\n')}
+      }
+    `;
+    let options = Object.assign({}, this.requestObject);
+    options.body = JSON.stringify({ query });
+    return fetch(this._url, options)
+      .then(res => res.json())
+      .then(response => (
+        new Promise((resolve, reject) => {
+          if (response.errors && response.errors.length) {
+            reject(response);
+          }
+          resolve(response);
+        })
+      ));
+  }
+}
+
+module.exports = FetchQL;
---
+++ test.js
@@ -0,0 +1,109 @@
+/**
+ * Created by gucheng on 5/18/16.
+ */
+var expect = require('chai').expect;
+var fetch = require('node-fetch');
+var FetchQL = require('./index.mjs');
+
+const testQuery = `
+  query Query {
+    user(username: "BlinkTunnel") {
+      name
+      id
+      avatar_url
+      html_url
+      type
+      name
+      company
+      blog
+      location
+      email
+      bio
+      public_repos
+      followers
+      following
+      repositories {
+        id
+        name
+        description
+        stargazers_count
+      }
+    }
+  }
+`;
+
+const testUrl = 'http://127.0.0.1:3000/another';
+const testQueryParams = {
+  operationName: 'Query',
+  query: testQuery,
+  variables: {}
+};
+
+var testQL = new FetchQL({
+  url: testUrl
+});
+
+describe('FetchQL', () => {
+  describe('#constructor', () => {
+    it('should return an Object', () => {
+      expect(testQL).to.be.a('object');
+    });
+  });
+  
+  describe('#methods', () => {
+    const methods = ['query', 'getUrl', 'setUrl', 'getEnumTypes'];
+    methods.map((method) => {
+      it(`${method}() should be a function`, () => {
+        expect(testQL[method]).to.be.a('function');
+      })
+    })
+  });
+  
+  describe('#getUrl()', () => {
+    let currentUrl = testQL.getUrl();
+    it(`should return url "${testUrl}"`, () => {
+      expect(currentUrl).to.be.a('string');
+      expect(currentUrl).to.eql(testUrl);
+    });
+  });
+
+  describe('#query()', () => {
+    let call = testQL.query(testQueryParams);
+    it('should return a Promise', () => {
+      expect(call).to.be.a('promise');
+    });
+    
+    it('should pass an object of response data in promise', () => {  
+      return call
+        .then(response => expect(response).to.be.a('object'))
+        .catch(error => expect(error).to.be.a('object'))
+    });
+  });
+  
+  describe('#getEnumTypes(["UserType"])', () => {
+    let call = testQL.getEnumTypes(['UserType']);
+    it('should return a Promise', () => {
+      expect(call).to.be.a('promise');
+    });
+    
+    it('should pass an Object cotains GraphQL enum type for "UserType" in promise', () => {
+      return call
+        .then(response => {
+          expect(response).to.be.a('object');
+          expect(response).to.have.property('UserType');
+          expect(response.UserType).to.have.all.keys('name', 'kinde', 'enumValues');
+        })
+        .catch(error => {
+          expect(error).to.be.a('object');
+        })
+    })
+  });
+
+  const newUrl = 'http://127.0.0.1:3000/graphql';
+  describe(`#setUrl("${newUrl}")`, () => {
+    it(`should set url to "${newUrl}"`, () => {
+      testQL.setUrl(newUrl);
+      expect(testQL.getUrl()).to.equal(newUrl);
+    });
+  });
+});
