f533f020747cd1bf42f393d1c3328ceca39d22f2
Cheng Gu <guchengf@gmail.com>
2016-08-08 12:21:44 +0800

    fix: error property name

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,48 +1,48 @@
 interface FetchQLInterceptor {
-    request: (url: string, config) => { url: string, config };
-    reueqstError: (error) => { reject: (error) => void };
-    response: (response) => Object;
-    responseError: (error) => { reject: (error) => void };
-  }
+  request: (url: string, config) => { url: string, config };
+  reueqstError: (error) => { reject: (error) => void };
+  response: (response) => Object;
+  responseError: (error) => { reject: (error) => void };
+}
 
-  interface FetchQLOptions {
-    url: string;
-    intercepotrs?: FetchQLInterceptor | FetchQLInterceptor[];
-    headers?: Object
-  }
+interface FetchQLOptions {
+  url: string;
+  intercepotrs?: FetchQLInterceptor | FetchQLInterceptor[];
+  headers?: Object
+}
 
-  interface FetchQLQuery {
-    operationName: string;
-    query: string;
-    variables?: Object;
-  }
+interface FetchQLQuery {
+  operationName: string;
+  query: string;
+  variables?: Object;
+}
 
-  interface FetchQL_Static {
-    intercepotrs: FetchQLInterceptor[];
+interface FetchQL_Static {
+  interceptors: FetchQLInterceptor[];
 
-    url: string;
+  url: string;
 
-    requestObject: Object;
+  requestObject: Object;
 
-    EnumMap: Object;
+  EnumMap: Object;
 
-    new (options: FetchQLOptions): FetchQL_Instance;
-  }
+  new (options: FetchQLOptions): FetchQL_Instance;
+}
 
-  interface FetchQL_Instance {
-    addInterceptors(intercepotrs: FetchQLInterceptor | FetchQLInterceptor[]): Function;
+interface FetchQL_Instance {
+  addInterceptors(interceptors: FetchQLInterceptor | FetchQLInterceptor[]): Function;
 
-    clearInterceptors(): void;
+  clearInterceptors(): void;
 
-    query(options: FetchQLQuery): { resolve: ({data, errors}) => void, rejedct: ({data, errors}) => void };
+  query(options: FetchQLQuery): { resolve: ({data, errors}) => void, rejedct: ({data, errors}) => void };
 
-    getEnumTypes(enums: string[]): { resolve: ({data, errors}) => void, rejedct: ({data, errors}) => void };
+  getEnumTypes(enums: string[]): { resolve: ({data, errors}) => void, rejedct: ({data, errors}) => void };
 
-    setUrl(): void;
+  setUrl(): void;
 
-    getUrl(): string;
-  }
+  getUrl(): string;
+}
 
-  declare var FetchQL: FetchQL_Static;
+declare var FetchQL: FetchQL_Static;
 
-  export default FetchQL;
+export default FetchQL;
