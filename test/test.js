684603eeb068c7971eaa5b72f059851cca6c5e40
gucheen <guchengf@gmail.com>
2016-12-06 22:06:26 +0800

    fix(test)

--- test/test.js
+++ test/test.js
@@ -30,6 +30,11 @@ const testQL = new FetchQL({
   headers: {
     'test-header': 'test-header',
   },
+  interceptors: [{
+    request(url, config) {
+      return [url, config];
+    },
+  }],
   onStart(requestQueueLength) {
     startTrack = true;
     queueLength = requestQueueLength;
@@ -76,7 +81,7 @@ describe('FetchQL', () => {
           expect(response.data).to.be.a('object');
           expect(response.data.testString).to.equal('It works!');
         })
-        .catch(error => expect(error).to.be.a('object'))
+        .catch(error => expect(error).to.be.a('error'));
     });
   });
 
@@ -90,11 +95,11 @@ describe('FetchQL', () => {
       return call
         .then(response => {
           expect(response).to.be.a('object');
-          expect(response).to.have.property('TestEnum');
-          expect(response.UserType).to.have.all.keys('name', 'kind', 'enumValues');
+          expect(response.data).to.have.property('TestEnum');
+          expect(response.data.TextEnum).to.have.all.keys('description', 'kind', 'enumValues');
         })
         .catch(error => {
-          expect(error).to.be.a('object');
+          expect(error).to.be.a('error');
         });
     });
   });
@@ -108,7 +113,7 @@ describe('FetchQL', () => {
           headerCheck
         }`,
       })
-        .then(({ data }) => {
+        .then(({data}) => {
           expect(data.headerCheck).to.be.true;
         });
     });
@@ -121,11 +126,11 @@ describe('FetchQL', () => {
   describe('Interceptor', () => {
     describe('#addInterceptors()', () => {
       removeInterceptors = testQL.addInterceptors({
-        request: function(url, config) {
+        request: function (url, config) {
           requestIntercepted = true;
           return [url, config];
         },
-        response: function(response) {
+        response: function (response) {
           responseIntercepted = true;
           return response;
         },
@@ -149,6 +154,14 @@ describe('FetchQL', () => {
     describe('#removeInterceptors', () => {
       it('should remove the added interceptors', () => {
         removeInterceptors();
+
+        expect(testQL.interceptors.length).to.equal(1);
+      });
+    });
+
+    describe('#clearInterceptors', () => {
+      it('should remove the added interceptors', () => {
+        testQL.clearInterceptors();
 
         expect(testQL.interceptors.length).to.equal(0);
       });
@@ -179,8 +192,10 @@ describe('FetchQL', () => {
             nullProp: null,
           },
         })
-          .then(() => {})
-          .catch(() => {});
+          .then(() => {
+          })
+          .catch(() => {
+          });
       });
     });
     describe('options of query({opts})', () => {
@@ -208,8 +223,10 @@ describe('FetchQL', () => {
             omitEmptyVariables: true,
           },
         })
-          .then(() => {})
-          .catch(() => {});
+          .then(() => {
+          })
+          .catch(() => {
+          });
       });
     });
   });
