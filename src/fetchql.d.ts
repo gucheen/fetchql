cdceff528a6aafeab25c8cc3b9debe96c1ad91f8
Jamee Kim <jameekim@nodaplife.me>
2018-09-03 12:35:24 +0900

    more details to typescript definitions

--- src/fetchql.d.ts
+++ src/fetchql.d.ts
@@ -1,27 +1,29 @@
 // Type definitions for FetchQL
 // Project: FetchQL
 // Definitions by: gucheen https://github.com/gucheen/
+//                 Jamee Kim https://github.com/jameekim
 
-export default  FetchQL;
+export default FetchQL;
 
 declare class FetchQL {
   constructor(options: FetchQLOptions);
-  
+
   private interceptors: FetchQLInterceptor[];
 
   private url: string;
 
   private requestObject: Object;
 
-  private EnumMap: Object;
+  private EnumMap: { [p: string]: FetchQLEnumInfo };
 
-  addInterceptors(interceptors: FetchQLInterceptor | FetchQLInterceptor[]): Function;
+  addInterceptors(interceptors: FetchQLInterceptor | FetchQLInterceptor[]): () => void;
 
   clearInterceptors(): void;
 
-  query(options: FetchQLQuery): Promise<{data, errors}>;
+  query<TData = any, TVariables = { [p: string]: any }>(options: FetchQLQuery<TVariables>):
+    Promise<FetchQLQueryResult<TData>>;
 
-  getEnumTypes(enums: string[]): Promise<{data, errors}>;
+  getEnumTypes(enums: string[]): Promise<FetchQLEnumResult>;
 
   setUrl(): void;
 
@@ -29,10 +31,10 @@ declare class FetchQL {
 }
 
 export interface FetchQLInterceptor {
-  request: (url: string, config) => { url: string, config };
-  reueqstError: (error) => Promise<{errors}>;
-  response: (response) => Object;
-  responseError: (error) => Promise<{errors}>;
+  request?: (url: string, config: any) => any;
+  requestError?: (error: any) => any;
+  response?: (response: Response | any) => Response | any;
+  responseError?: (error: any) => any;
 }
 
 export interface FetchQLOptions {
@@ -45,12 +47,44 @@ export interface FetchQLOptions {
   requestOptions?: Object;
 }
 
-export interface FetchQLQuery {
+export interface FetchQLQuery<TVariables = { [p: string]: any }> {
   operationName: string;
   query: string;
-  variables?: Object;
+  variables?: TVariables;
   opts?: {
     omitEmptyVariables: boolean;
   };
   requestOptions?: Object;
+}
+
+export interface FetchQLQueryResult<TData = any> {
+  data: TData;
+  errors?: Array<FetchQLErrorInfo | GraphQLError>;
+}
+
+export interface FetchQLEnumResult {
+  data: { [p: string]: FetchQLEnumInfo | null };
+  errors?: FetchQLErrorInfo[];
+}
+
+export interface FetchQLEnumInfo {
+  kind: 'ENUM';
+  description: string;
+  enumValues: Array<string | number>;
+}
+
+export interface FetchQLErrorInfo {
+  message: string;
+  stack: Response;
+}
+
+interface GraphQLError {
+  message: string;
+  locations?: any;
+  path?: any;
+  nodes?: any;
+  source?: any;
+  positions?: any;
+  originalError?: any;
+  extensions?: any;
 }
